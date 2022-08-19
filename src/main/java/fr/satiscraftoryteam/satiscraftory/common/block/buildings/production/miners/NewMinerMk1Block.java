6a23f0033a0d5df6e51e51e69bf9245e9e9c8b16
vgtom4 <tom.vieillard.grelet@gmail.com>
2022-08-19 18:32:05 +0200

    Added interface to NewMinerMk1Block + face direction (unlinked hitbox).

--- src/main/java/fr/satiscraftoryteam/satiscraftory/common/block/buildings/production/miners/NewMinerMk1Block.java
+++ src/main/java/fr/satiscraftoryteam/satiscraftory/common/block/buildings/production/miners/NewMinerMk1Block.java
@@ -5,20 +5,51 @@ import fr.satiscraftoryteam.satiscraftory.common.block.base.BoudingAttribute;
 import fr.satiscraftoryteam.satiscraftory.common.block.base.MachineBaseBlock;
 import fr.satiscraftoryteam.satiscraftory.common.block.base.ShapeAttribute;
 import fr.satiscraftoryteam.satiscraftory.common.init.TileEntityInit;
+import fr.satiscraftoryteam.satiscraftory.common.interfaces.IHasTickableTileEntity;
+import fr.satiscraftoryteam.satiscraftory.common.registry.TileEntityRegistryObject;
 import fr.satiscraftoryteam.satiscraftory.common.shapes.ShapesList;
+import fr.satiscraftoryteam.satiscraftory.common.tileentity.BlockEntityutils;
 import fr.satiscraftoryteam.satiscraftory.common.tileentity.MinerMk1BlockEntity;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.core.Vec3i;
+import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Mirror;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.DirectionProperty;
 import net.minecraft.world.level.material.Material;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
-public class NewMinerMk1Block extends MachineBaseBlock {
+import java.util.EnumMap;
+import java.util.Map;
 
+public class NewMinerMk1Block extends MachineBaseBlock implements IHasTickableTileEntity {
+
+    private static final Vec3i P2OFFSET = new Vec3i(0, 0, 3);
 
     public NewMinerMk1Block() {
         super(BlockBehaviour.Properties.of(Material.METAL));
+        //registerDefaultState(this.defaultBlockState().setValue(FACING, Direction.NORTH));
 
         blockProps.add(new ShapeAttribute(ShapesList.MINER_MK1));
         blockProps.add(new BoudingAttribute((pos, state, builder) -> {
@@ -34,6 +65,30 @@ public class NewMinerMk1Block extends MachineBaseBlock {
         }));
     }
 
+    //------------------------------------------BLOCK_ENTITY----------------------------------------------------------//
+
+    @Override
+    public RenderShape getRenderShape(BlockState blockState) {
+        return RenderShape.INVISIBLE;
+    }
+
+    @Override
+    public void onRemove(BlockState blockState, Level level, BlockPos blockPos, BlockState pNewState, boolean pIsMoving) {
+        if (blockState.getBlock() != pNewState.getBlock()) {
+            BlockEntity blockEntity = level.getBlockEntity(blockPos);
+            if (blockEntity instanceof MinerMk1BlockEntity) {
+                ((MinerMk1BlockEntity) blockEntity).drops();
+            }
+        }
+        super.onRemove(blockState, level, blockPos, pNewState, pIsMoving);
+    }
+
+
+    @Override
+    public TileEntityRegistryObject<? extends MinerMk1BlockEntity> getTileType() {
+        return TileEntityInit.MINER_MK1_BLOCK_ENTITY;
+    }
+
     @Nullable
     @Override
     public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
@@ -44,4 +99,64 @@ public class NewMinerMk1Block extends MachineBaseBlock {
     public BlockProps getType() {
         return super.blockProps;
     }
+    //----------------------------------------------------------------------------------------------------------------//
+
+    //---------------------------------------------DirectionFace------------------------------------------------------//
+
+    public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+
+    @Override
+    protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> builder) {
+        super.createBlockStateDefinition(builder);
+        builder.add(FACING);
+    }
+
+    @Override
+    public BlockState rotate(BlockState blockState, Rotation rotation) {
+        return blockState.setValue(FACING, rotation.rotate(blockState.getValue(FACING)));
+    }
+
+    @Override
+    public BlockState mirror(BlockState blockState, Mirror mirror) {
+        return blockState.rotate(mirror.getRotation(blockState.getValue(FACING)));
+    }
+
+    @Nullable
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext context) {
+        return this.defaultBlockState().setValue(FACING, context.getHorizontalDirection().getOpposite());
+    }
+
+    //----------------------------------------------------------------------------------------------------------------//
+
+
+    //------------------------------------------OPEN_INTERFACE--------------------------------------------------------//
+    @Override
+    public InteractionResult use(BlockState blockState, Level level, BlockPos blockPos, Player player, InteractionHand hand, BlockHitResult blockHitResult) {
+        if (!level.isClientSide()) {
+            BlockEntity entity = level.getBlockEntity(blockPos);
+            if(entity instanceof MinerMk1BlockEntity) {
+                NetworkHooks.openScreen(((ServerPlayer)player), (MinerMk1BlockEntity)entity, blockPos);
+            } else {
+                throw new IllegalStateException("Our Container provider is missing!");
+            }
+        }
+        return InteractionResult.sidedSuccess(level.isClientSide());
+    }
+    //----------------------------------------------------------------------------------------------------------------//
+
+    //------------------------------------------------particle--------------------------------------------------------//
+    public static boolean particle = true;
+
+    @Override
+    public void animateTick(BlockState blockState, Level level, BlockPos blockPos, RandomSource randomSource) {
+        float chance = 0.35f;
+        if (chance < randomSource.nextFloat() & particle) {
+            level.addParticle(ParticleTypes.FLAME, blockPos.getX() + randomSource.nextFloat(), blockPos.getY() + 1D, blockPos.getZ() + randomSource.nextFloat(), 0d, 0.05d, 0d);
+            level.addParticle(ParticleTypes.CAMPFIRE_SIGNAL_SMOKE, blockPos.getX() + randomSource.nextFloat(), blockPos.getY() + 5D, blockPos.getZ() + randomSource.nextFloat(), 0d, 0.05d, 0d);
+        }
+
+        super.animateTick(blockState, level, blockPos, randomSource);
+    }
+    //----------------------------------------------------------------------------------------------------------------//
 }
