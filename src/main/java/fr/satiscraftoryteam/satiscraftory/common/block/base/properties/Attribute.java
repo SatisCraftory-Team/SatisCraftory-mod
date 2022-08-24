1c74d7068dd49f49067db7b3605ddb3491a60c01
manugame <manugameur.pro@gmail.com>
2022-08-24 12:07:29 +0200

    StateAttribute and IOAttribute

--- src/main/java/fr/satiscraftoryteam/satiscraftory/common/block/base/properties/Attribute.java
+++ src/main/java/fr/satiscraftoryteam/satiscraftory/common/block/base/properties/Attribute.java
@@ -6,6 +6,7 @@ import fr.satiscraftoryteam.satiscraftory.common.tileentity.base.MachineBaseTile
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 
+import java.lang.constant.Constable;
 import java.util.Collection;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
