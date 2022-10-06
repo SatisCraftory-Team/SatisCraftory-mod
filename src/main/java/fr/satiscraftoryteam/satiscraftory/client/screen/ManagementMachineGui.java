dcb36a31c3dbd8c74c549c0cfcc79160eb0ced01
vgtom4 <tom.vieillard.grelet@gmail.com>
2022-10-06 20:34:38 +0200

    attempt to isolate secondary GUI parts (overclock and informations)

---
+++ src/main/java/fr/satiscraftoryteam/satiscraftory/client/screen/ManagementMachineGui.java
@@ -0,0 +1,43 @@
+package fr.satiscraftoryteam.satiscraftory.client.screen;
+
+import com.mojang.blaze3d.systems.RenderSystem;
+import com.mojang.blaze3d.vertex.PoseStack;
+import fr.satiscraftoryteam.satiscraftory.SatisCraftory;
+import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
+import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Inventory;
+import net.minecraft.world.inventory.AbstractContainerMenu;
+
+public class ManagementMachineGui extends AbstractContainerScreen {
+    private static final ResourceLocation INVENTORY =
+            new ResourceLocation(SatisCraftory.MODID, "textures/gui/inventory.png");
+    private static final ResourceLocation CONFIG_BAR =
+            new ResourceLocation(SatisCraftory.MODID, "textures/gui/config_bar.png");
+
+    public ManagementMachineGui(AbstractContainerMenu menu, Inventory inventory, Component component) {
+        super(menu, inventory, component);
+    }
+
+    @Override
+    protected void renderBg(PoseStack poseStack, float PartialTick, int pMouseX, int pMouseY) {
+        RenderSystem.setShader(GameRenderer::getPositionTexShader);
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+
+        int x = (width - imageWidth) / 2;
+        int y = (height - imageHeight) / 2;
+
+        this.blit(poseStack, x, y, 0, 0, imageWidth, imageHeight);
+
+        RenderSystem.setShaderTexture(0, CONFIG_BAR);
+        this.blit(poseStack, x-100, y+10, 0, 0, 100, 60);
+        this.blit(poseStack, x+imageWidth, y+10, 106, 60, 176, 120);
+
+
+        RenderSystem.setShaderTexture(0, INVENTORY);
+        this.blit(poseStack, x, y + 100, 0, 0, 176, 100);
+
+
+    }
+}
