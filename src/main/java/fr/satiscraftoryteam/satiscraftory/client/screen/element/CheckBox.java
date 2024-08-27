0c0da0e4260edd2fc031a3d0772101f5384e187e
manugame <manugameur.pro@gmail.com>
2024-08-27 21:16:40 +0200

    menu: fix checkbox

--- src/main/java/fr/satiscraftoryteam/satiscraftory/client/screen/element/CheckBox.java
+++ src/main/java/fr/satiscraftoryteam/satiscraftory/client/screen/element/CheckBox.java
@@ -1,8 +1,10 @@
 package fr.satiscraftoryteam.satiscraftory.client.screen.element;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import fr.satiscraftoryteam.satiscraftory.SatisCraftory;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.AbstractWidget;
+import net.minecraft.client.gui.components.AbstractButton;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.resources.sounds.SimpleSoundInstance;
 import net.minecraft.client.sounds.SoundManager;
@@ -13,7 +15,7 @@ import net.neoforged.api.distmarker.Dist;
 import net.neoforged.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
-public class CheckBox extends AbstractWidget
+public class CheckBox extends AbstractButton
 {
     private static final ResourceLocation BUTTON_ON_OFF = ResourceLocation.fromNamespaceAndPath(SatisCraftory.MODID, "textures/gui/components.png");
 
@@ -34,24 +36,25 @@ public class CheckBox extends AbstractWidget
         return this.toggled;
     }
 
-//    @Override
-//    public void renderButton(PoseStack poseStack, int mouseX, int mouseY, float partialTicks)
-//    {
-//        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
-//        RenderSystem.setShaderTexture(0, BUTTON_ON_OFF);
-//        this.blit(poseStack, this.x, this.y, 0, 0, 8, 8);
-//        if(!this.toggled)
-//        {
-//            this.blit(poseStack, this.getX(), this.getY(), 8, 0, 9, 8);
-//        }
-//        drawString(poseStack, Minecraft.getInstance().font, this.getMessage(), this.x + 12, this.y, 0xFFFFFF);
-//    }
 
-    private int oof;
 
     @Override
-    protected void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+    public void renderWidget(GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTicks)
+    {
+        RenderSystem.setShaderColor(1.0F, 1.0F, 1.0F, 1.0F);
+        RenderSystem.setShaderTexture(0, BUTTON_ON_OFF);
+        guiGraphics.blit(BUTTON_ON_OFF, this.getX(), this.getY(), 0, 0, 8, 8);
+        if(!this.toggled)
+        {
+            guiGraphics.blit(BUTTON_ON_OFF, this.getX(), this.getY(), 8, 0, 9, 8);
+        }
+        guiGraphics.drawString(Minecraft.getInstance().font, this.getMessage(), this.getX() + 12, this.getY(), 0xFFFFFF);
+    }
 
+
+    @Override
+    public void onPress() {
+        this.toggled = !this.toggled;
     }
 
     @Override
--- src/main/java/fr/satiscraftoryteam/satiscraftory/common/init/MenuTypesInit.java
+++ src/main/java/fr/satiscraftoryteam/satiscraftory/common/init/MenuTypesInit.java
@@ -5,7 +5,6 @@ import fr.satiscraftoryteam.satiscraftory.client.screen.BuilderToolMenu;
 import fr.satiscraftoryteam.satiscraftory.client.screen.MinerMk1Menu;
 import fr.satiscraftoryteam.satiscraftory.client.screen.SmelterMenu;
 import net.minecraft.core.registries.Registries;
-import net.minecraft.world.flag.FeatureFlags;
 import net.minecraft.world.inventory.MenuType;
 import net.neoforged.neoforge.common.extensions.IMenuTypeExtension;
 import net.neoforged.neoforge.registries.DeferredRegister;
@@ -19,7 +18,7 @@ public class MenuTypesInit {
     public static final Supplier<MenuType<MinerMk1Menu>> MINER_MK1_MENU = MENUS.register("miner_mk1_menu", () -> IMenuTypeExtension.create(MinerMk1Menu::new));
     public static final Supplier<MenuType<SmelterMenu>> SMELTER_MENU = MENUS.register( "smelter_menu", () -> IMenuTypeExtension.create(SmelterMenu::new));
 
-   public static final Supplier<MenuType<BuilderToolMenu>> BUILDER_TOOL_MENU = MENUS.register("builder_tool", () -> new MenuType<>(BuilderToolMenu::new, FeatureFlags.DEFAULT_FLAGS));
+   public static final Supplier<MenuType<BuilderToolMenu>> BUILDER_TOOL_MENU = MENUS.register("builder_tool", () -> IMenuTypeExtension.create(BuilderToolMenu::new));
 
 //    private static <T extends AbstractContainerMenu>RegistryObject<MenuType<T>> registerMenuType(IContainerFactory<T> factory, String name) {
 //        return MENUS.register(name, () -> IForgeMenuType.create(factory));
