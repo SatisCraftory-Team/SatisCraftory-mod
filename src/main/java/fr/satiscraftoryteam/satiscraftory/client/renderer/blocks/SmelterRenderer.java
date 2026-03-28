7e7cadad8ebc421d77e2ee11f946ff5de5c16342
vgtom4 <tom.vieillard.grelet@gmail.com>
2026-03-28 23:48:04 +0100

    feat: override getRenderBoundingBox method to use dynamic bounding box for SmelterBlockEntity

--- src/main/java/fr/satiscraftoryteam/satiscraftory/client/renderer/blocks/SmelterRenderer.java
+++ src/main/java/fr/satiscraftoryteam/satiscraftory/client/renderer/blocks/SmelterRenderer.java
@@ -3,10 +3,16 @@ package fr.satiscraftoryteam.satiscraftory.client.renderer.blocks;
 import fr.satiscraftoryteam.satiscraftory.client.models.blocks.SmelterModel;
 import fr.satiscraftoryteam.satiscraftory.common.tileentity.SmelterBlockEntity;
 import net.minecraft.client.renderer.blockentity.BlockEntityRendererProvider;
+import net.minecraft.world.phys.AABB;
 import software.bernie.geckolib.renderer.GeoBlockRenderer;
 
 public class SmelterRenderer extends GeoBlockRenderer<SmelterBlockEntity> {
     public SmelterRenderer(BlockEntityRendererProvider.Context context) {
         super(new SmelterModel());
+    }
+
+    @Override
+    public AABB getRenderBoundingBox(SmelterBlockEntity blockEntity) {
+        return blockEntity.getDynamicRenderBoundingBox();
     }
 }
